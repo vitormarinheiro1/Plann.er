48293d911bf7feeefb7f08ce0007d37ecb43b27d
Vitor <vitormarinheiro9@gmail.com>
2024-07-09 20:45:35 -0300

    feat: useNavigate para voltar ao clicar no botao alterar local/data

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,7 +1,9 @@
 import { Calendar, MapPin, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
+import { useNavigate } from "react-router-dom";
 
 export function DestinationAndDateHeader() {
+  const navigate = useNavigate()
   return (
     <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
       <div className="flex items-center gap-2">
@@ -20,7 +22,7 @@ export function DestinationAndDateHeader() {
 
         <div className="w-px h-6 bg-zinc-800" />
 
-        <Button variant="secondary">
+        <Button onClick={() => navigate('/')} variant="secondary">
           Alterar local/data
           <Settings2 className="size-5" />
         </Button>
