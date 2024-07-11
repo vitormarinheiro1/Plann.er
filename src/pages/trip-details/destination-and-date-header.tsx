7c835ffd2d5b7be57e743f6d072f1149d976f6b9
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-11 15:28:20 -0300

    feat: adicionando dados da api no header e nos convidados

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,14 +1,37 @@
 import { Calendar, MapPin, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
 import { useNavigate } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import { api } from "../../lib/axios";
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+
+interface Trip {
+  id: string
+  destination: string
+  starts_at: string
+  ends_at: string
+  is_confirmed: boolean
+}
 
 export function DestinationAndDateHeader() {
   const navigate = useNavigate()
+  
+  const { tripId } = useParams()
+  const [trip, setTrip] = useState<Trip | undefined>()
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}`)
+      .then(response => setTrip(response.data.trip))
+  }, [tripId])
+
+  const displayedDate = trip ? format(trip.starts_at, "d' de 'LLL").concat(' até ').concat(format(trip.ends_at, "d' de 'LLL")) : null
+
   return (
     <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
       <div className="flex items-center gap-2">
         <MapPin className="size-5 text-zinc-400" />
-        <span className="text-zinc-100">Florianóplois, Brasil</span>
+        <span className="text-zinc-100">{trip?.destination}</span>
       </div>
 
 
@@ -16,7 +39,7 @@ export function DestinationAndDateHeader() {
         <div className="flex items-center gap-2">
           <Calendar className="size-5 text-zinc-400" />
           <span className="text-zinc-100">
-            17 a 23 de Agosto
+            {displayedDate}
           </span>
         </div>
 
--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,30 +1,46 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+interface Participants {
+  id: string
+  name: string | null
+  email: string
+  is_confirmed: boolean
+}
 
 export function Guests() {
+  const { tripId } = useParams()
+  const [participants, setParticipants] = useState<Participants[]>([])
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/participants`)
+      .then(response => setParticipants(response.data.participants))
+  }, [tripId])
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5 flex-1">
-            <span className="block font-medium text-zinc-100">Jessia White</span>
-            <span className="block text-sm text-zinc-400 truncate">
-              jessica.white44@gmail.com
-            </span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5" />
-        </div>
-
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5 flex-1">
-            <span className="block font-medium text-zinc-100">Dra. Rita Pacocha</span>
-            <a className="block text-sm text-zinc-400 truncate">
-              lacy.stiedemann@gmail.com
-            </a>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5" />
-        </div>
+        {participants.map((participant, index) => {
+          return (
+            <div key={participant.id} className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5 flex-1">
+                <span className="block font-medium text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
+                <span className="block text-sm text-zinc-400 truncate">
+                  {participant.email}
+                </span>
+              </div>
+              {participant.is_confirmed ? (
+                <CheckCircle2 className="size-5 shrink-0 text-green-400" />
+              ) : (
+                <CircleDashed className="text-zinc-400 size-5" />
+              )}
+            </div>
+          )
+        })}
       </div>
 
       <Button variant="secondary" size="full">
